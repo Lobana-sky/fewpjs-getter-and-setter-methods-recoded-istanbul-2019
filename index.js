0531c9153efb4e5a1e4cc4a42a00c10738777f3b
lobana alarabi <loubanaeng@gmail.com>
2020-02-15 00:12:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
 // Add your Circle class here
+class Circle{
+  constructor(radius){
+    this.radius=radius;
+  }
+  
+  get diameter(){
+    return this.radius*2;
+  } 
+  get circumference(){
+    return Math.PI* this.radius*2;
+  } 
+  get area(){
+    return Math.PI* this.radius*this.radius;
+  }
+  
+  
+  
+  set diameter(dia){
+    this._radius=dia/2;
+  } 
+  set circumference(cir){
+    
+    this._radius=cir/(Math.PI*2);
+  } 
+  set area(ara){
+    this.radius=Math.sqrt(ara/Math.PI);
+    
+  }
+  
+}
